20510453eea6508066c70165952230756d23be97
Marino Bonetti <marinobonetti@gmail.com>
2024-04-11 09:34:40 +0200

    test: Base spec permitted editable attributes

--- spec/support/base_editing_controller_helpers.rb
+++ spec/support/base_editing_controller_helpers.rb
@@ -215,3 +215,19 @@ RSpec.shared_examples "a validated? object" do
     it { is_expected.to be_validated }
   end
 end
+
+
+RSpec::Matchers.define :permit_editable_attributes do |*expected_attributes|
+  description { "to permit editable attributes:#{expected_attributes}" }
+  failure_message { "'#{actual}' does not permit attributes #{expected_attributes - actual.editable_attributes}" }
+  match do |actual|
+    actual_attributes = expected_attributes - actual.editable_attributes
+
+    actual_attributes.empty?
+  end
+  match_when_negated do |actual|
+    actual_attributes = expected_attributes & actual.editable_attributes
+
+    actual_attributes.empty?
+  end
+end
