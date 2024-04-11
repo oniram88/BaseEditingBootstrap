8a96534386c87fda787a30dc1c8c1734018d4945
Marino Bonetti <marinobonetti@gmail.com>
2024-04-11 07:23:40 +0200

    test: Spostamento shared example in posto corretto

--- spec/support/base_editing_controller_helpers.rb
+++ spec/support/base_editing_controller_helpers.rb
@@ -137,3 +137,15 @@ RSpec.shared_examples "base editing controller" do |factory: nil, only: [], exce
     end
   end
 end
+
+
+default_unathorized_failure = -> { raise "TODO - passare proc con richiesta che dovrà fallire" }
+
+RSpec.shared_examples "fail with unauthorized" do |request: default_unathorized_failure|
+  it "expect to redirect to root" do
+    expect(Pundit).to receive(:authorize).with(user, any_args).and_raise(Pundit::NotAuthorizedError)
+    instance_exec(&request)
+    expect(response).to redirect_to(root_path)
+    expect(flash[:error]).not_to be_nil
+  end
+end
