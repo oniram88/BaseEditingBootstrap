a6b8c49cd7ae6f4e600c6caf45ea0d57e0f14c9a
Marino Bonetti <marinobonetti@gmail.com>
2024-04-15 14:48:58 +0200

    feat: Generalizzazione campi rispetto al tipo di dato

--- app/helpers/utilities/form_helper.rb
+++ app/helpers/utilities/form_helper.rb
@@ -16,19 +16,37 @@ module Utilities
     # @param [Forms::Base] form
     # @param [Symbol] field
     def form_print_field(form, field)
-      generic_field = case field
-                      when :created_at, :updated_at
-                        "timestamps"
-                      else
-                        "base"
-                      end
+      locals = {form:, field:}
+      case form.object.class.type_for_attribute(field).type
+      when :datetime
+         generic_field = "datetime_field"
+      when :date
+         generic_field = "date_field"
+      when :decimal
+        locals[:scale] = form.object.class.type_for_attribute(field).scale || 2
+        generic_field = "decimal_field"
+      when :float
+        locals[:scale] = 2 #usiamo il default dato che non abbiamo questa informazione negli attributes di rails
+        generic_field = "decimal_field"
+      when :integer
+        generic_field = "integer_field"
+      else
+        case field
+        when :created_at, :updated_at
+          "timestamps"
+        else
+          generic_field = "base"
+        end
+      end
+
       template = find_template_with_fallbacks(
         form.object,
         field,
         "form_field",
         generic_field
       )
-      render template, form:, field:
+      Rails.logger.debug { "#{template}->#{ locals.inspect}" }
+      render template, **locals
     end
 
   end
