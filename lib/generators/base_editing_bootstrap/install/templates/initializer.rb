ba90fbaa6bfec65bbc996a02768e7a71d2c59dea
Marino Bonetti <marinobonetti@gmail.com>
2024-05-21 21:58:43 +0200

    fix: Correct initializer commented examples

--- lib/generators/base_editing_bootstrap/install/templates/initializer.rb
+++ lib/generators/base_editing_bootstrap/install/templates/initializer.rb
@@ -5,18 +5,18 @@ BaseEditingBootstrap.configure do |config|
   # Controller da cui derivare poi il BaseEditingController da cui derivano
   # tutti i controller sottostanti
   # @default "ApplicationController"
-  # config_accessor :inherited_controller, default: "ApplicationController"
+  #  config.inherited_controller = "ApplicationController"
 
   ##
   # Configurazione per alterare lo standard di azione post aggiornamento record
   # il default è andare nella pagina di editing del record
   # possibili valori :edit , :index
-  # config_accessor :after_success_update_redirect, default: :edit
+  # config.inherited_controller = :edit
 
   ##
   # Configurazione per alterare lo standard di azione post creazione record
   # il default è andare nella pagina di editing del record
   # possibili valori :edit , :index
-  # config_accessor :after_success_create_redirect, default: :edit
+  # config.inherited_controller = :edit
 
 end
