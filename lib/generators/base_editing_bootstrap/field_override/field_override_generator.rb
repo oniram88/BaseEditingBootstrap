cd3f40a4bdf2e6f09afcfbd9f09969aa07fc68d9
Marino Bonetti <marinobonetti@gmail.com>
2024-05-29 23:07:20 +0200

    test: Add Tests to generator

--- lib/generators/base_editing_bootstrap/field_override/field_override_generator.rb
+++ lib/generators/base_editing_bootstrap/field_override/field_override_generator.rb
@@ -6,17 +6,24 @@ module BaseEditingBootstrap
       source_root File.expand_path("../../../../app/views/base_editing", __dir__)
       argument :attributes, type: :array, default: [], banner: "field field:type"
 
-      desc "copy partial files for a defined fields, the possible types are [date,datetime,decimal,enum,integer]"
-
+      desc <<-DESCRIPTION.strip_heredoc
+             Description:   
+                Copy partial files for a defined fields, 
+                the possible types are [date,datetime,decimal,integer]
+                C'è anche enum, ma per quella dobbiamo fare a mano
+      DESCRIPTION
       def copy_form_partials
-        raise "Need one field" if attributes_names.empty?
-        attributes.each do |a|
-          type = a.type
-          type = :base if type == :string
-          copy_file "form_field/_#{type}.html.erb", File.join("app/views", plural_name, singular_name, 'form_field', "_#{a.name}.html.erb")
+        if attributes_names.empty?
+          say "Need one field"
+        else
+          attributes.each do |a|
+            type = a.type
+            type = :base if type == :string
+            copy_file "form_field/_#{type}.html.erb", File.join("app/views", plural_name, singular_name, 'form_field', "_#{a.name}.html.erb")
+          end
         end
-      end
 
+      end
     end
   end
 end
---
+++ spec/lib/generators/field_override_generator_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+require 'generators/base_editing_bootstrap/field_override/field_override_generator'
+
+RSpec.describe BaseEditingBootstrap::Generators::FieldOverrideGenerator, type: :generator do
+  destination File.join(ENGINE_ROOT, "spec/dummy/tmp/generators_spec")
+
+  before do
+    prepare_destination
+  end
+
+  describe "Combinations override fields" do
+    where(:type, :source) do
+      [
+        [nil, "_base"],
+        ["date", "_date"],
+        ["datetime", "_datetime"],
+        ["decimal", "_decimal"],
+        #["enum", "_enum"], TODO capire come implementare anche enum
+        ["integer", "_integer"]
+      ]
+    end
+
+    with_them do
+      it "should create a copy" do
+        source_path = File.join(ENGINE_ROOT, "app/views/base_editing/form_field/#{source}.html.erb")
+        run_generator ["Post", "title:#{type}"]
+        expect(destination_root).to have_structure {
+          directory("app/views/posts/post/form_field") do
+            file("_title.html.erb") do
+              contains File.read(source_path)
+            end
+          end
+        }
+      end
+    end
+  end
+
+  it "should create multiple in one shot" do
+    run_generator ["Post", "title", "name:date", "created_at:datetime"]
+    expect(destination_root).to have_structure {
+      directory("app/views/posts/post/form_field") do
+        file("_title.html.erb")
+        file("_name.html.erb")
+        file("_created_at.html.erb")
+      end
+    }
+  end
+
+end
