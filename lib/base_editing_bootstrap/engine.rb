30f862d64c6d9726d84686607dfd0188c2c30ebd
Marino Bonetti <marinobonetti@gmail.com>
2024-05-31 22:36:32 +0200

    chore: Remove useless configuration

--- lib/base_editing_bootstrap/engine.rb
+++ lib/base_editing_bootstrap/engine.rb
@@ -1,12 +1,6 @@
 module BaseEditingBootstrap
   class Engine < ::Rails::Engine
 
-    config.generators do |g|
-      g.test_framework :rspec
-      g.fixture_replacement :factory_bot
-      g.factory_bot dir: 'spec/factories'
-    end
-
     initializer "base_editing_bootstrap.deprecator" do |app|
       app.deprecators[:base_editing_bootstrap] = BaseEditingBootstrap.deprecator
     end
