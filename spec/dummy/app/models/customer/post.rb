27793c453bb7d8eb27954fa4639154379cfea653
Jury Ghidinelli <jury.ghidinelli@archimedianet.it>
2024-07-25 11:10:11 +0200

    fix: Bad copy and past

--- spec/dummy/app/models/customer/post.rb
+++ spec/dummy/app/models/customer/post.rb
@@ -1,20 +1,5 @@
 module Customer
   class Post < Post
-    include BaseEditingBootstrap::BaseModel
-    validates :title, presence: true
-
-    enum :category, {
-      news: "news",
-      project: "project",
-      other: "other"
-    }
-
-    enum :priority, {
-      normal: 0,
-      low: 1,
-      urgent: 2
-    }, default: :normal, prefix: true
-
 
     def this_is_special_method_for_customer_post
       "this_is_special_method_for_customer_post"
