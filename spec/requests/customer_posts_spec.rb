19b1fb7427f32fd01ba9c9865a346d2b63043149
Marino Bonetti <marinobonetti@gmail.com>
2024-10-14 07:30:20 +0200

    test: Build structure for testing

--- spec/requests/customer_posts_spec.rb
+++ spec/requests/customer_posts_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Customer::Posts", type: :request do
+RSpec.describe "Controller Namespaced Class", type: :request do
   it_behaves_like "as logged in user" do
-    let(:user) { create(:user) }
-    it_behaves_like "base editing controller", factory: :post
 
-
-    describe "show" do
+    describe "namespaced resources with namespaced model" do
       let(:post) { create(:post) }
-      it "get" do
+      it "get correct namespaced class" do
         get customer_post_path(post.id)
         expect(assigns(:object)).to be_an_instance_of(Customer::Post)
         expect(response.body).to have_tag(:span, text: /this_is_special_method_for_customer_post/)
       end
 
+    end
+
+    describe "namespaced resources withouth namespaced model" do
+      it "get correct class" do
+        get customer_users_path()
+        expect(response).to have_http_status(:ok)
+        expect(assigns(:_base_class)).to be == User
+      end
     end
 
 
