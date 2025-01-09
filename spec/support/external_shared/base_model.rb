8348d830b3b67a7e7887be0fa61dfb59e4e11cc2
Marino Bonetti <marinobonetti@gmail.com>
2025-01-09 17:38:26 +0100

    fix: Spec helper clear code and optimization to base model checks

--- spec/support/external_shared/base_model.rb
+++ spec/support/external_shared/base_model.rb
@@ -6,48 +6,39 @@ RSpec.shared_examples "a base model" do |ransack_permitted_attributes: [], ransa
     expect(described_class).to respond_to(:human_action_message)
   end
 
+  ##
+  # Oggetto solitamente di classe User che identifichi l'utente a cui eseguire il check dei permessi
+  let(:auth_object) { :auth_object }
+  let(:new_user_ransack_permitted_attributes) { ransack_permitted_attributes }
+  let(:new_user_ransack_permitted_associations) { ransack_permitted_associations }
+
   describe "with ransackables" do
-    where(:base_model_method, :policy_method, :policy_output, :result) do
+    where(:base_model_method, :result, :new_user_result) do
       [
         [
-          :ransackable_attributes, :permitted_attributes_for_ransack, ransack_permitted_attributes.map(&:to_s), ransack_permitted_attributes.map(&:to_s)
+          :ransackable_attributes, ransack_permitted_attributes.map(&:to_s), ref(:new_user_ransack_permitted_attributes)
         ],
         [
-          :ransackable_attributes, :permitted_attributes_for_ransack, ransack_permitted_attributes.map(&:to_sym), ransack_permitted_attributes.map(&:to_s)
+          :ransackable_associations, ransack_permitted_associations.map(&:to_s), ref(:new_user_ransack_permitted_associations)
         ],
-        [
-          :ransackable_associations, :permitted_associations_for_ransack,ransack_permitted_associations.map(&:to_s), ransack_permitted_associations.map(&:to_s)
-        ],
-        [
-          :ransackable_associations, :permitted_associations_for_ransack, ransack_permitted_associations.map(&:to_sym), ransack_permitted_associations.map(&:to_s)
-        ]
       ]
     end
 
     with_them do
-      subject { described_class.send(base_model_method, auth_object) }
+      subject { described_class.send(base_model_method, inner_auth_object) }
 
-      let(:simulated_user_instance) { instance_double("User") }
-      # before do
-      #   allow(User).to receive(:new).and_return(simulated_user_instance)
-      # end
-      let(:auth_object) { nil }
-      let(:policy) {
-        instance_double("BaseModelPolicy", policy_method => policy_output)
-      }
+      let(:inner_auth_object) { nil }
       it "new user" do
         expect(Pundit).to receive(:policy).with(an_instance_of(User),
                                                 an_instance_of(described_class)).and_call_original
-        #.and_return(policy)
 
-        is_expected.to match_array(result)
+        is_expected.to match_array(new_user_result)
       end
 
       context "with auth_object" do
-        let(:auth_object) { :auth_object }
+        let(:inner_auth_object) { auth_object }
         it do
-          expect(Pundit).to receive(:policy).with(auth_object, an_instance_of(described_class)).and_call_original
-          # .and_return(policy)
+          expect(Pundit).to receive(:policy).with(inner_auth_object, an_instance_of(described_class)).and_call_original
 
           is_expected.to match_array(result)
         end
