8d00d481c169991048098236be1e4d72acd5cb67
Marino Bonetti <marinobonetti@gmail.com>
2025-01-15 11:07:03 +0100

    test: Add current_user for helpers

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,10 +1,23 @@
-
-
-
-
 RSpec.shared_context "as logged in user" do
   let(:user) { create(:user) }
   before {
-    user #così sono sicuro di generarlo
+    user # così sono sicuro di generarlo
   }
+end
+
+module SimulatedCurrentUser
+  def current_user
+    @current_user ||= User.new
+  end
+end
+module SimulatedCurrentUserHelper
+
+  def self.included(base)
+    base.described_class.include SimulatedCurrentUser
+    super
+  end
+end
+
+RSpec.configure do |config|
+  config.include SimulatedCurrentUserHelper, type: :helper
 end
