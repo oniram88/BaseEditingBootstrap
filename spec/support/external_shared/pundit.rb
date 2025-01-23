15690c7a7258a58147aa79ea0fde0c534de40667
Marino Bonetti <marinobonetti@gmail.com>
2025-01-23 12:20:47 +0100

    feat: Add new test helper for association relation

--- spec/support/external_shared/pundit.rb
+++ spec/support/external_shared/pundit.rb
@@ -37,6 +37,47 @@ RSpec.shared_examples "a standard base model policy" do |factory, check_default_
     end
   end
 
+  # Controllo che se negli elementi dei #sortable_search_result_fields è presente un elemento
+  # che ha come prefisso il nome di una relazione presente in #permitted_associations_for_ransack
+  # allora nella policy della relativa associazione dovrò trovare in #permitted_attributes_for_ransack
+  # il nome della colonna da ordinare.
+  # ES:
+  # relazione: User -> Post
+  # class PostPolicy
+  #   def sortable_search_result_fields = %i[user_username]
+  #   def permitted_associations_for_ransack = %i[user]
+  # end
+  # class UserPolicy
+  #   def permitted_attributes_for_ransack = %[username]
+  # end
+  # ordinamento per #user_username, quindi devo cercare relazione in `user` e attribute `username`
+  it "check associated sortable attributes" do
+    klass = instance.record.class
+    # non facciamo nulla se non è un active record il record da controllare
+    if klass.respond_to?(:reflect_on_association)
+
+      # escludiamo le colonne del modello
+      col_to_check = (instance.sortable_search_result_fields.collect(&:to_s) - instance.record.class.column_names).uniq
+
+      elenco_campi_ordinabili_in_relazione = col_to_check.map do |field|
+        instance.permitted_associations_for_ransack.map do |rel|
+          if field.match(/^#{rel}_(.*)$/)
+            [rel, Regexp.last_match(1)]
+          end
+        end
+      end.flatten(1).compact.uniq
+
+      elenco_campi_ordinabili_in_relazione.each do |relation,field|
+        reflection = klass.reflect_on_association(relation.to_s)
+        policy = Pundit.policy(instance.user, reflection.class_name.constantize.new)
+        expect(policy.permitted_attributes_for_ransack.collect(&:to_sym).include?(field.to_sym)).to be_truthy, lambda{
+          "Mi aspetto che `#{policy.class.name}#permitted_attributes_for_ransack` includa `#{field}` per permettere l'ordinamento del campo tramite relazione"
+        }
+      end
+
+    end
+  end
+
   describe "standard_methods" do
     where(:method, :response) do
 
