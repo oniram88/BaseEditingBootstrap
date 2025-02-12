3ba5fc221e11a2053047556aa4c65a739a041e5c
Marino Bonetti <marinobonetti@gmail.com>
2025-02-12 11:28:26 +0100

    refactor: Rimozione inutile codice

--- spec/dummy/spec/policies/comment_policy_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-RSpec.describe CommentPolicy, type: :policy do
-    it_behaves_like "a standard base model policy", :comment
-
-end
--- spec/dummy/spec/requests/comments_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-RSpec.describe "Comments", type: :request do
-  it_behaves_like "as logged in user" do
-    it_behaves_like "base editing controller", factory: :comment
-  end
-end
