526a4106fbdff1f3d88f464af17903a7f55c4770
Marino Bonetti <marinobonetti@gmail.com>
2025-02-12 11:30:14 +0100

    feat: Skip polymorphic for belongs to

--- spec/dummy/app/policies/comment_policy.rb
+++ spec/dummy/app/policies/comment_policy.rb
@@ -1,13 +1,7 @@
 class CommentPolicy < BaseModelPolicy
 
-  def editable_attributes = %i[comment commentable_id]
-  def permitted_attributes = %i[comment commentable_id]
-  def search_result_fields = %i[comment commentable_id]
-  def search_fields
-    %i[comment_i_cont]
-  end
-  # TODO check if correct with search_fields
-  def permitted_attributes_for_ransack
-    %i[comment]
-  end
+  def editable_attributes = %i[comment commentable]
+  def permitted_attributes = %i[comment]
+  def search_result_fields = %i[comment]
+
 end
